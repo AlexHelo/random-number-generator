af6b03eb7acccccb936d78d71e35e526d405eccc
Alex Helo <itsalexhelo@gmail.com>
2022-04-16 01:11:26 -0500

    Added Styled & Added Method Selector

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+      <ButtonGroup variant="outlined" aria-label="outlined button group">
+        <Button>MCC</Button>
+        <Button>MC</Button>
+        <Button>MCM</Button>
+        <Button>GM</Button>
+        <Button>MClC</Button>
+      </ButtonGroup>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
